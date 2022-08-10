4f8bef1ba5bcd2681fd1b798905e66b8f24b5e9b
Jens Debernitz <jens.debernitz@cgm.com>
2022-08-10 20:48:44 +0200

    added a lot of unctions

--- DrinkOMat/DrinkOMat.ino
+++ DrinkOMat/DrinkOMat.ino
@@ -6,156 +6,258 @@
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); // Set the LCD I2C address UNO
 unsigned long idleTimerMillis = 0;
-// ############ specific configurations ####################
-
-// max = number of buttons (default: 5; even with 1,2 or 3 rows of 5 compartments); if you want to have a button for each compartment increase up to 15 or more
-// don't forget to look at EEPROM structure when changing value of max
-const int max = 5; 
-// number of rows (1 = 5 compartments, 2 = 10 compartments, 3 = 15 compartments)
-// if maxrow <=2 the button #5 is used to start refill programm otherwise for open 3rd row during programming
-const int maxrow = 1;
-
-unsigned long idlePeriod = 150000; // time in ms between idle messages or shutdown e.g. 180000  
-
-// debug modus: if you need 10 vendors and one additonal PIN (TX0 / Digital PIN 1 which should normally not be used) on UNO turn debug mode to 0 which disables serial 
-// I use this pin to send a message to my smarthome system for any purchased product
+unsigned long idlePeriod = 150000; // time in ms between idle messages or shutdown e.g. 180000
 bool debug = true;
 
-// ########## INIT VALUES ##########
-// #max
-// #PREIS
-int conveyorPrice[5] = {500, 500, 500, 500, 500}; // default price  
-int conveyorItems[5] = {1, 1, 1, 1, 1};
-
-// ########## INIT COIN ACCEPTOR ##########
-
-const int coinInt = 0; // attach coinInt to interrupt pin 0 = digital pin 2 = digitalPinToInterrupt(2) . (interrupt pin 1 = digital pin 3 = digitalPinToInterrupt(3))
-
-// set the coinsCurrentValue to a volatile float
-// volatile as this variable changes any time the Interrupt is triggered
+const int coinIntteruptPin = 0; // attach coinInt to interrupt pin 0 = digital pin 2 = digitalPinToInterrupt(2) . (interrupt pin 1 = digital pin 3 = digitalPinToInterrupt(3))
 volatile int coinsCurrentValue = 0;
 int coinsChange = 0; // a coin has been inserted flag
-
 unsigned long currentMillis = 0;
 unsigned long oldMillis = 0;
-
 int pulsecount;
 
-// const int relaisPin = xx; //
-const int relays[5] = {3, 4, 5, 6, 7}; //uno: 5 or 10 boxes
 
-const int selector[5] = {17, 16, 15, 14, 0 }; // input pins for selector buttons (A0=14, A1=15, A2=16, A3=17, Dig0=0) UNO
 
-// PINS
-const int configbutton = 13;   // 13 for Uno
-const int refillbutton = 12;  // for Uno 12 (if only one row of compartments exists: maxrow = 1)
-const int powersave_relais_pin = 11;  // for Uno 11
+const int relayK1AuswurfTrommel = 3;
+const int relayK2Schubladenverschluss = 4;
+const int relayK3LedBeleuchtung = 5;
 
-void setup() 
+const int switchS1GrundstellungAuswurfTrommel = 6;
+const int switchS2SchubladeVerschlossen = 7;
+const int switchS3LetzteFlasche = 8;
+const int switchS4BestellTaster = 9;
+
+//private functions
+bool pinBuffer = false;
+void initPorts();
+void auswurfTrommelZurGrundstellung();
+void writeTextOnLcd(const char* message);
+
+void setup()
 {
-  if (debug) 
+  if (debug)
   {
-    Serial.begin(9600); // start serial communication
-    Serial.println("Max: ");
-    Serial.println(max);
+    Serial.begin(19200); // start serial communication
   }
 
- for (int index = 0; index < max; index++) 
- {
-    pinMode(selector[index], INPUT_PULLUP);
- }
- 
-  pinMode(configbutton, INPUT_PULLUP);
-  pinMode(refillbutton, INPUT_PULLUP);  
+  initPorts();
 
-  lcd.begin(16, 2);
-  lcd.print("Gude Drink-O-Mat");
+  lcd.begin(20, 4);
+  lcd.clear();
+  lcd.backlight();
+  writeTextOnLcd("Start...");
 
-  if(debug)
+  if (debug)
   {
     Serial.println("Wait on...");
   }
 
   delay(2000);
 
-  attachInterrupt(coinInt, coinInserted, RISING);
+  writeTextOnLcd("Trommel Reset");
+  auswurfTrommelZurGrundstellung();
 
-  for (int index = 0; index < (max * maxrow); index++) 
+  if (debug)
   {
-    digitalWrite(relays[index], HIGH); // Turn OFF
-    pinMode(relays[index], OUTPUT);
+    Serial.println("Bereit");
   }
+
+  pulsecount = 0;
+  attachInterrupt(coinIntteruptPin, coinInserted, RISING);
 
   delay(200);
   coinsCurrentValue = 0;
-  lcd.print("Guthaben reset.");
   delay(200);
-
-  if (debug) 
-  {
-    Serial.println("Bereit");
-  }  
-
-  lcd.clear();
-  lcd.print("Bereit");
 }
 
-void loop() 
+void loop()
 {
-  // put your main code here, to run repeatedly:
 
+  if (debug)
+  {
+    static unsigned int lastStateLetzteFlasche = -1;
+
+    if (lastStateLetzteFlasche != digitalRead(switchS3LetzteFlasche))
+    {
+      char buffer[100];
+      sprintf(&buffer[0], "Flaschen Leer %i", digitalRead(switchS3LetzteFlasche));
+      Serial.println(&buffer[0]);
+      lastStateLetzteFlasche = digitalRead(switchS3LetzteFlasche);
+    }
+
+  }
+  digitalWrite(relayK3LedBeleuchtung, HIGH);
+  digitalWrite(relayK2Schubladenverschluss, HIGH);
+  digitalWrite(relayK1AuswurfTrommel, HIGH);
+
+  if (digitalRead(switchS3LetzteFlasche) == LOW)
+  {
+    int coinsBig = coinsCurrentValue / 100;
+    int coinsLess = coinsCurrentValue % 100 ;
+    char buffer[20];
+    sprintf(&buffer[0], "Guthaben %i,%i      ", coinsBig, coinsLess);
+    writeTextOnLcd(&buffer[0]);
+
+    //wenn der münzzähler mehr als 99Cent aingeworfen wurden und der bestelltaster betätigt wurde dann lege los
+    if (coinsCurrentValue >= 100)
+    {
+      if (digitalRead(switchS4BestellTaster) == LOW)
+      {
+        coinsCurrentValue -= 100;
+
+        while (1)
+        {
+          if (digitalRead(switchS2SchubladeVerschlossen) == LOW)
+          {
+            writeTextOnLcd("Bier ausgabe        ");
+            digitalWrite(relayK1AuswurfTrommel, LOW);
+            delay(500);
+            while (digitalRead(switchS1GrundstellungAuswurfTrommel) == HIGH)
+            {
+              //do so long to zero position
+              if (debug)
+              {
+                Serial.println("Fahre Trommel bis null punkt");
+              }
+            }
+            digitalWrite(relayK1AuswurfTrommel, HIGH);
+
+            digitalWrite(relayK3LedBeleuchtung, LOW);
+            digitalWrite(relayK2Schubladenverschluss, LOW);
+            delay(500);
+            while (digitalRead(switchS2SchubladeVerschlossen) == HIGH)
+            {
+              digitalWrite(relayK2Schubladenverschluss, HIGH);
+              if (debug)
+              {
+                Serial.println("warte so lange bis schublade wieder geschlossen wurde");
+              }
+            }
+            digitalWrite(relayK3LedBeleuchtung, HIGH);
+
+            writeTextOnLcd("Viel Spaß beim trinken");
+            delay(1000);
+            break;
+          }
+          else
+          {
+            writeTextOnLcd("Schublade schliessen");
+
+            if (debug)
+            {
+              Serial.println("schublade ist nicht geschlossen");
+            }
+          }
+        }
+
+      }
+    }
+  }
+  else
+  {
+    writeTextOnLcd("Leider leer :(      ");
+  }
+
+  if (coinsChange == 1)
+  {
+    coinsChange = 0; // unflag that a coin has been inserted
+  }
 }
 
-// ########## COIN INSERT ##########
 
-// This function is called by interrupt every time we receives a pulse from the coin acceptor
-void coinInserted() 
+void writeTextOnLcd(const char* message)
+{
+
+  lcd.setCursor(0, 0);
+  lcd.print("*** Drink-O-MMat ***");
+  lcd.setCursor(0, 1);
+  lcd.print("Gude");
+  lcd.setCursor(0, 2);
+  lcd.print("Lust auf ein Bier?");
+  lcd.setCursor(0, 3);
+  lcd.print(message);
+}
+
+void auswurfTrommelZurGrundstellung()
+{
+  if (debug)
+  {
+    char buffer[100];
+    sprintf(&buffer[0], "Reset Auswurftrommel %i", digitalRead(switchS1GrundstellungAuswurfTrommel));
+    Serial.println(&buffer[0]);
+  }
+
+  while (digitalRead(switchS1GrundstellungAuswurfTrommel) == HIGH)
+  {
+    digitalWrite(relayK1AuswurfTrommel, LOW);
+  }
+
+  digitalWrite(relayK1AuswurfTrommel, HIGH);
+}
+
+void initPorts()
+{
+  pinMode(switchS1GrundstellungAuswurfTrommel, INPUT_PULLUP);
+  pinMode(switchS2SchubladeVerschlossen, INPUT_PULLUP);
+  pinMode(switchS3LetzteFlasche, INPUT_PULLUP);
+  pinMode(switchS4BestellTaster, INPUT_PULLUP);
+   pinMode(2, INPUT_PULLUP);
+
+
+  digitalWrite(relayK1AuswurfTrommel, HIGH);
+  pinMode(relayK1AuswurfTrommel, OUTPUT);
+  digitalWrite(relayK2Schubladenverschluss, HIGH);
+  pinMode(relayK2Schubladenverschluss, OUTPUT);
+  digitalWrite(relayK3LedBeleuchtung, HIGH);
+  pinMode(relayK3LedBeleuchtung, OUTPUT);
+}
+
+void coinInserted()
 {
   coinsChange = 1; // flag that there has been a coin inserted
 
   currentMillis = millis();
   int difference = currentMillis - oldMillis;
 
-  //  Serial.print("difference: ");
-  if (debug) 
+  if (debug)
   {
-    Serial.println(difference); 
-  } 
+    char buffer[100];
+    sprintf(&buffer[0], "Int PIN %i --> Different %i --> ", digitalRead(2), difference);
+    Serial.print(&buffer[0]);
+  }
+
   oldMillis = currentMillis;
   pulsecount++;
 
-// new coin? start to count from beginning....   
-if (difference > 140 or difference < 50 ) 
-{
-    if (pulsecount > 1) 
-    {
-       pulsecount = 1;
-     }
-   }
-
-  if (difference < 141) 
+  // new coin? start to count from beginning....
+  if (difference > 140 || difference < 50 )
   {
-    switch (pulsecount) 
+    if (pulsecount > 1)
     {
-    case 2: coinsCurrentValue += 10;  
-        break; 
-    case 3: coinsCurrentValue += 10;  
-        break; 
-    case 4: coinsCurrentValue += 30;  
-        break; 
-    case 5: coinsCurrentValue += 50;  
-        break; 
-    case 6: coinsCurrentValue += 100;  
-        break;         
+      pulsecount = 1;
     }
   }
-  
+
+  if (difference < 141)
+  {
+    switch (pulsecount)
+    {
+      case 2: coinsCurrentValue += 20;
+        break;
+      case 3: coinsCurrentValue += 30;
+        break;
+      case 4: coinsCurrentValue += 50;
+        break;
+      case 5: coinsCurrentValue += 100;
+        break;
+    }
+  }
+
   idleTimerMillis = millis();
-  
-  if (debug) 
-  {    
+
+  if (debug)
+  {
     Serial.print ("(/coinsInserted)neuer Wert: ");
     Serial.println (  coinsCurrentValue);
   }
 }
-  
