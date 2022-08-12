96449298883de6125629e0f599611bdc4637d86d
Jens Debernitz <jens.debernitz@cgm.com>
2022-08-12 05:46:37 +0200

    added a lot of comments

--- DrinkOMat/DrinkOMat.ino
+++ DrinkOMat/DrinkOMat.ino
@@ -1,14 +1,18 @@
 #include <Wire.h>
 #include <LCD.h>
 #include <LiquidCrystal_I2C.h>
-#include <EEPROM.h>
 
-
+//initialisierung LCD Display
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); // Set the LCD I2C address UNO
+
+//timer einstellungen
 unsigned long idleTimerMillis = 0;
 unsigned long idlePeriod = 150000; // time in ms between idle messages or shutdown e.g. 180000
+
+//Serielle ausgaben
 bool debug = true;
 
+//alles was man für den münzprüfer braucht
 const int coinIntteruptPin = 0; // attach coinInt to interrupt pin 0 = digital pin 2 = digitalPinToInterrupt(2) . (interrupt pin 1 = digital pin 3 = digitalPinToInterrupt(3))
 volatile int coinsCurrentValue = 0;
 int coinsChange = 0; // a coin has been inserted flag
@@ -16,32 +20,38 @@ unsigned long currentMillis = 0;
 unsigned long oldMillis = 0;
 int pulsecount;
 
-
-
+//Ports für die Releais
 const int relayK1AuswurfTrommel = 3;
 const int relayK2Schubladenverschluss = 4;
 const int relayK3LedBeleuchtung = 5;
 
+//Ports f+r die schalter
 const int switchS1GrundstellungAuswurfTrommel = 6;
 const int switchS2SchubladeVerschlossen = 7;
 const int switchS3LetzteFlasche = 8;
 const int switchS4BestellTaster = 9;
 
+//andere konstanten
+const int BOTTLE_PRICE = 100; //in Cent
+const char* VERSION_STRING = "0.1 Beta";
+
 //private functions
-bool pinBuffer = false;
 void initPorts();
 void auswurfTrommelZurGrundstellung();
 void writeTextOnLcd(const char* message);
 
 void setup()
 {
+  //init der seriellen Ausgabe
   if (debug)
   {
     Serial.begin(19200); // start serial communication
   }
 
+  //init Ports
   initPorts();
 
+  //init LCD
   lcd.begin(20, 4);
   lcd.clear();
   lcd.backlight();
@@ -54,6 +64,7 @@ void setup()
 
   delay(2000);
 
+  //Auswurf Trommel zur Grundstellung zurück fahren
   writeTextOnLcd("Trommel Reset");
   auswurfTrommelZurGrundstellung();
 
@@ -62,6 +73,7 @@ void setup()
     Serial.println("Bereit");
   }
 
+  //init Interrupt Pin für den Münzprüfer
   pulsecount = 0;
   attachInterrupt(coinIntteruptPin, coinInserted, RISING);
 
@@ -73,6 +85,7 @@ void setup()
 void loop()
 {
 
+  //ausgabe wenn sich der Letzte Flaschen Schaler geändert hat
   if (debug)
   {
     static unsigned int lastStateLetzteFlasche = -1;
@@ -86,14 +99,19 @@ void loop()
     }
 
   }
+
+  //wenn in main Loop können wir die Releais immer ausschalten
   digitalWrite(relayK3LedBeleuchtung, HIGH);
   digitalWrite(relayK2Schubladenverschluss, HIGH);
   digitalWrite(relayK1AuswurfTrommel, HIGH);
 
+  //prüfung ob der letzte Flaschen schalter gechaltet wurde
   if (digitalRead(switchS3LetzteFlasche) == LOW)
   {
+    //anzeige des guthabens
     int coinsBig = coinsCurrentValue / 100;
-    int coinsLess = coinsCurrentValue % 100 ;
+    //% = Modulo = gibt dir den rest wert einer diffusion = 1112 % 11 wäre dann 12 ;-)
+    int coinsLess = coinsCurrentValue % 100;
     char buffer[20];
     sprintf(&buffer[0], "Guthaben %i,%i      ", coinsBig, coinsLess);
     writeTextOnLcd(&buffer[0]);
@@ -101,17 +119,25 @@ void loop()
     //wenn der münzzähler mehr als 99Cent aingeworfen wurden und der bestelltaster betätigt wurde dann lege los
     if (coinsCurrentValue >= 100)
     {
+      //Bestelltaster wurde betätigt
       if (digitalRead(switchS4BestellTaster) == LOW)
       {
-        coinsCurrentValue -= 100;
+        //flaschen preis vom guthaben abziehen
+        coinsCurrentValue -= BOTTLE_PRICE;
 
         while (1)
         {
+          //es könnte sein das der vortrinker die schublade nicht richtig verschlossen hat,
+          //das sollte man abwarten
           if (digitalRead(switchS2SchubladeVerschlossen) == LOW)
           {
+
+            //wenn die schublade richtigverschlossen ist können wir die auswurftrommel anwerfen bis sie eine volle drehung gemacht hat
             writeTextOnLcd("Bier ausgabe        ");
             digitalWrite(relayK1AuswurfTrommel, LOW);
             delay(500);
+
+            //hier warten wir bis die trommel wieder in der grundeinstellung ist
             while (digitalRead(switchS1GrundstellungAuswurfTrommel) == HIGH)
             {
               //do so long to zero position
@@ -122,9 +148,12 @@ void loop()
             }
             digitalWrite(relayK1AuswurfTrommel, HIGH);
 
+            //licht an und schublade auf
             digitalWrite(relayK3LedBeleuchtung, LOW);
             digitalWrite(relayK2Schubladenverschluss, LOW);
             delay(500);
+
+            //so lange warten bis die schublade wieder zu ist
             while (digitalRead(switchS2SchubladeVerschlossen) == HIGH)
             {
               digitalWrite(relayK2Schubladenverschluss, HIGH);
@@ -165,19 +194,22 @@ void loop()
 }
 
 
+//Text ausgabe auf dem LCD
 void writeTextOnLcd(const char* message)
 {
-
   lcd.setCursor(0, 0);
   lcd.print("*** Drink-O-MMat ***");
   lcd.setCursor(0, 1);
-  lcd.print("Gude");
+  char buffer[20];
+  sprintf(&buffer[0], "Gude %s ", VERSION_STRING);
+  lcd.print(&buffer[0]);
   lcd.setCursor(0, 2);
   lcd.print("Lust auf ein Bier?");
   lcd.setCursor(0, 3);
   lcd.print(message);
 }
 
+//Hilfsfunktion für die grundeinstellung der auswurftrommel
 void auswurfTrommelZurGrundstellung()
 {
   if (debug)
@@ -195,13 +227,14 @@ void auswurfTrommelZurGrundstellung()
   digitalWrite(relayK1AuswurfTrommel, HIGH);
 }
 
+//hilfsfunktion zur initialisieung der Ports
 void initPorts()
 {
   pinMode(switchS1GrundstellungAuswurfTrommel, INPUT_PULLUP);
   pinMode(switchS2SchubladeVerschlossen, INPUT_PULLUP);
   pinMode(switchS3LetzteFlasche, INPUT_PULLUP);
   pinMode(switchS4BestellTaster, INPUT_PULLUP);
-   pinMode(2, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
 
 
   digitalWrite(relayK1AuswurfTrommel, HIGH);
@@ -212,6 +245,7 @@ void initPorts()
   pinMode(relayK3LedBeleuchtung, OUTPUT);
 }
 
+//interrupt funktion zum zählen der Implulse vom Münzprüfer
 void coinInserted()
 {
   coinsChange = 1; // flag that there has been a coin inserted
